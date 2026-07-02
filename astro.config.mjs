d3dba4404991c6754e005fbd7b399b534be58626
leozhao <swhmsoft@gmail.com>
2026-07-02 08:51:27 +0800

    URL斜杠

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://cnc.bozemetal.com',
   output: 'static',
+  trailingSlash: 'ignore',
   integrations: [
     react(),
     sitemap({
